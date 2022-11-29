03c81e06d12d0131435706668893d3150915aeac
Vernon Pitney <vernon.pitney@mrjaffesclass.com>
2022-11-29 10:55:22 -0800

    Complete

--- DieN.java
+++ DieN.java
@@ -1,19 +1,20 @@
 
-
 public class DieN {
-  private int value;
-  public DieN() {
-    this.roll();
-  }
-  public int getValue() {
-    return value;
-  }
-  public void roll() {
-    this.value = (int)(Math.random() * 20) + 1;
-  }
-  public int rollAndGetValue() {
-    roll();
-    return getValue();
-  }
+   private int value;
+   private int sides;
+    public DieN(int sides) {
+       this.sides = sides;
+       this.roll();
+   }
+   public int getValue() {
+       return value;
+   }
+   public void roll() {
+       this.value = (int)(Math.random() * this.sides) + 1;
+   }
+   public int rollAndGetValue() {
+       this.roll();
+       return getValue();
+   }
 }
 
